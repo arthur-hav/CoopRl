1227ae316141511c0d9c2be0b557e51ea6337856
Arthur Havlicek <arthurhavlicek@gmail.com>
2017-09-23 16:06:19 +0200

    Principe génération

---
+++ dungeon-gen.py
@@ -0,0 +1,8 @@
+#1 - Place rooms. This can be done using map presets.
+
+#2 - Carve a maze with remaining walls. A maze is basically a DFS but with random son ordering. -> a bias can be ordered to favor straight halls.
+
+#3 - Iterate through rooms and open 1 to 2 doors for each to the generated maze.
+
+
+
